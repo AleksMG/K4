d698a786d7fe4847833c15ad851e849a28320352
AleksMG <5978497@gmail.com>
2025-05-14 22:34:23 +0300

    Create dictionary.js

---
+++ dictionary.js
@@ -0,0 +1,26 @@
+// English frequency data
+const FrequencyDictionaries = {
+    ENGLISH: {
+        'A': 0.08167, 'B': 0.01492, 'C': 0.02782, 'D': 0.04253,
+        'E': 0.12702, 'F': 0.02228, 'G': 0.02015, 'H': 0.06094,
+        'I': 0.06966, 'J': 0.00153, 'K': 0.00772, 'L': 0.04025,
+        'M': 0.02406, 'N': 0.06749, 'O': 0.07507, 'P': 0.01929,
+        'Q': 0.00095, 'R': 0.05987, 'S': 0.06327, 'T': 0.09056,
+        'U': 0.02758, 'V': 0.00978, 'W': 0.02360, 'X': 0.00150,
+        'Y': 0.01974, 'Z': 0.00074
+    }
+};
+
+// Expanded common words list
+const CommonWords = [
+    'THE', 'AND', 'THAT', 'HAVE', 'FOR', 'NOT', 'WITH', 'YOU', 'THIS', 'BUT',
+    'HIS', 'FROM', 'THEY', 'WILL', 'WOULD', 'THERE', 'THEIR', 'WHAT', 'ABOUT',
+    'WHICH', 'WHEN', 'CAN', 'YOUR', 'SOME', 'COULD', 'THEM', 'SEE', 'LIKE',
+    'THEN', 'OTHER', 'WERE', 'TIME', 'LOOK', 'TWO', 'MORE', 'GO', 'WAY',
+    'CAME', 'THAN', 'ITS', 'OVER', 'ONLY', 'AFTER', 'MANY', 'ANY', 'MAKE',
+    'BACK', 'THROUGH', 'YEARS', 'WHERE', 'MUCH', 'BEFORE', 'DOWN', 'SHOULD',
+    'BECAUSE', 'EVEN', 'THOSE', 'PEOPLE', 'WELL', 'MIGHT', 'STILL', 'OWN',
+    'JUST', 'STATE', 'HERE', 'BOTH', 'BETWEEN', 'NEED', 'EACH', 'THESE',
+    'MOST', 'WHILE', 'WHERE', 'AGAIN', 'SUCH', 'FEW', 'DURING', 'UNDER',
+    'PLACE', 'WITHOUT', 'NORTH', 'EAST', 'CLOCK', 'UNTIL', 'BERLIN'
+];
